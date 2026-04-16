f8144c6f491b57d8de69add022fb173982362f44
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-04-16 09:18:08 +0530

    Routes setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import MyAppointment from './pages/MyAppointment'
+import MyProfile from './pages/MyProfile'
+import PageNotFound from './pages/PageNotFound'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
+    <h1 className="mx-4 sm-mx-[10%]">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-appointment' element={<MyAppointment />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/*' element={<PageNotFound />} />
+
+      </Routes>
   </h1>
   )
 }
