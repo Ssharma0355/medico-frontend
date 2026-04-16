d2286869f74ef235b8afdb56b79411fd67130a03
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-04-16 11:31:09 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import Login from './pages/Login'
 import MyAppointment from './pages/MyAppointment'
 import MyProfile from './pages/MyProfile'
 import PageNotFound from './pages/PageNotFound'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <h1 className="mx-4 sm-mx-[10%]">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/my-appointment' element={<MyAppointment />} />
         <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointment' element={<MyAppointment />} />
+        <Route path='/my-appointment/:doctorid' element={<Appointment />} />
         <Route path='/*' element={<PageNotFound />} />
-
       </Routes>
   </h1>
   )
